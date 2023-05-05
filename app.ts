5e9297d91a6bf9507dc9d656ab5474e2cf6a5807
MinoThauros <herimino@timatech.ca>
2023-05-05 12:36:39 -0400

    base working setup

---
+++ app.ts
@@ -0,0 +1,5 @@
+export function greeter(name: string) {
+    console.log(`Hello, ${name}!`);
+  }
+  
+  greeter("World");
---
+++ dist/app.js
@@ -0,0 +1,5 @@
+export function greeter(name) {
+    console.log(`Hello, ${name}!`);
+}
+greeter("World");
+//# sourceMappingURL=app.js.map
